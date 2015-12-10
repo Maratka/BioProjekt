184ec7231d025596d7bc86e9d7c86b87839d99cd
Marta Czarnowska <m.a.czarnowska@gmail.com>
2015-12-10 15:38:25 +0100

    Tree validation

--- projekt1/Main.py
+++ projekt1/Main.py
@@ -1,7 +1,46 @@
-import dendropy
+#-*- coding: utf-8 -*-
 
-tree1 = dendropy.Tree.get(
-        path="../tree.txt",
+import dendropy
+import re
+
+def loadData(path):
+    """
+    param: path - scieżka do pliku
+    return: tree - drzewo w formacie newick (string)
+    """
+    file = open(path, 'r')
+    tree = file.read()
+    file.close()
+    return tree
+
+def checkTree(tree):
+    """ Sprawdza zgodność rodziny klastrów
+    param: tree - drzewo w formacie newick (string)
+    return: boolean
+    """
+    #nazwy węzłów
+    tree = re.findall(r"[a-zA-Z]+", tree)
+
+    #liczba wystapień każdej nazwy
+    nodesCount = {}
+    for node in tree:
+        if node in nodesCount:
+            return False
+        else:
+            nodesCount[node] = 1
+
+    return True
+
+
+newickTree = loadData('../tree.txt')
+if not checkTree(newickTree):
+    print("Rodzina klastrów nie jest zgodna.")
+    exit()
+else:
+    print("Rodzina klastrów jest zgodna.")
+
+tree = dendropy.Tree.get(
+        data=newickTree,
         schema="newick")
 
-tree1.print_plot()
+tree.print_plot()
