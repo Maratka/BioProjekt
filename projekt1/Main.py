d68613d7a701503e8af9a8d68471ea1e4ed671f3
malsolec <malsolec@student.pg.gda.pl>
2015-12-13 21:08:26 +0100

    refactor

--- projekt1/Main.py
+++ projekt1/Main.py
@@ -9,6 +9,7 @@ from projekt1.Comparer import Comparer
 from projekt1.Converter import Converter
 from projekt1.Cluster_Tree import Cluster_Tree
 from projekt1.break_converter import BreakConverter
+from projekt1 import consensus_tree_file
 
 def loadData(path):
     """
@@ -45,43 +46,6 @@ def checkTree(tree):
     return True
 
 
-def find_consensus_tree(trees, precent):
-
-    consensus_tree = None
-    cluster_trees = []
-    num_of_trees = len(trees)
-    # Tworzenie wszystkich rozbić drzewa i wstawianie ich do tablicy
-    for tree in trees:
-        cluster_tree = Cluster_Tree(tree)
-        cluster_trees.append(cluster_tree)
-
-    # Porówanie każdgo elementu z tablicy z innymi tablicami
-    for cluster_tree in cluster_trees:
-        for other_cluster_tree in cluster_trees:
-            if other_cluster_tree is not cluster_tree:
-                for cluster in other_cluster_tree.get_cluster_list():
-                    cluster_tree.count_if_contain_the_same(cluster)
-
-    # Zliczenie tych, które powtarzaja sie wiecej niz x% razy i dodanie ich do drzewa konsensusu
-    num_of_similar = float(percent) * num_of_trees
-    # wybranie tych klastrow z kazdego dzrzewa, ktor emaja wiecej niz percent procent zgodnosci i nie powtarzaja sie
-    consensus_tree = Cluster_Tree(None)
-    for cluster_tree in cluster_trees:
-        for cluster in cluster_tree.get_cluster_list():
-            if cluster.found >= num_of_similar:
-                if not cluster_is_in_tree(cluster, consensus_tree):
-                    consensus_tree.add_cluster_to_cluster_list(cluster)
-
-    # przekonvertowanie tego drzewa na dendropy
-    consensus_tree_dendropy = Converter().get_dendropy_tree(consensus_tree)
-
-    return consensus_tree_dendropy
-
-def cluster_is_in_tree(cluster, cluster_tree):
-    for cluster_in_tree in cluster_tree.get_cluster_list():
-        if Comparer().are_the_same(cluster_in_tree,cluster):
-            return True
-    return False
 
 if __name__ == "__main__":
     percent = sys.argv.pop()
@@ -126,7 +90,7 @@ if __name__ == "__main__":
             distance = break_trees[break_tree_index].get_rf_distance(break_trees[break_tree_index+1])
             print(str(distance))
 
-    consensus_tree = find_consensus_tree(trees, percent)
+    consensus_tree = consensus_tree_file.find_consensus_tree(trees, percent)
     print("Consensus tree \n\n")
     draw.drawTree(consensus_tree)
 
